f87b8c5b85748093ec83ade594ebf4df0bd11956
Atilla <atillaseyid87@gmail.com>
2021-05-24 11:43:52 +0500

    Add files via upload
    add test

---
+++ junitTest.java
@@ -0,0 +1,20 @@
+import org.w3c.dom.Document;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class junitTest extends Application{
+   
+    public static void main(String[] args) {
+        Application.launch(junitTest.class, args);
+    }
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+    	
+    	TicTacToe tic = new TicTacToe();
+    	
+    	
+        
+    }
+}
