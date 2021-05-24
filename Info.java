a31ebfc7ef1583e3b90ea1877b3b72c4b60dfbf7
Atilla <atillaseyid87@gmail.com>
2021-05-24 11:43:11 +0500

    Add files via upload
    info object

---
+++ Info.java
@@ -0,0 +1,20 @@
+
+public class Info {
+	
+	private String FirstName ="";
+	private String SecondName ="";
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getSecondName() {
+		return SecondName;
+	}
+	public void setSecondName(String secondName) {
+		SecondName = secondName;
+	}
+	
+
+}
